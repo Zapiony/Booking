de123ab4a609609b8e25db3d8056bd16ba857a21
Dass-A <codedass.0@gmail.com>
2026-04-20 19:42:45 -0500

    Update(IUnitOfWork)
    Agregado campo para Clientes en el IUnitOfWork

--- Microservicio.Booking.DataManagement/Interfaces/IUnitOfWork.cs
+++ Microservicio.Booking.DataManagement/Interfaces/IUnitOfWork.cs
@@ -10,9 +10,16 @@ namespace Microservicio.Booking.DataManagement.Interfaces;
 /// </summary>
 public interface IUnitOfWork : IDisposable
 {
+ 
+    // Usuarios
     IUsuarioRepository UsuarioRepository { get; }
     IRolRepository RolRepository { get; }
     UsuarioQueryRepository UsuarioQueryRepository { get; }
 
+    // Clientes
+    IClienteRepository ClienteRepository { get; }
+    ClienteQueryRepository ClienteQueryRepository { get; }
+
+    // Persistencia
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
